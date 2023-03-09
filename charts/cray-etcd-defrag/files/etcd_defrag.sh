2567eeb5e21750b3cef210ad7def8dbb47747241
Brad Klein <bklein@cray.com>
2023-03-09 13:49:40 -0700

    Refactor defrag process for bitnami etcd chart

--- charts/cray-etcd-defrag/files/etcd_defrag.sh
+++ charts/cray-etcd-defrag/files/etcd_defrag.sh
@@ -1,6 +1,27 @@
 #!/bin/sh
-# Copyright 2020 Hewlett Packard Enterprise Development LP
-
+#
+# MIT License
+#
+# (C) Copyright 2020-2023 Hewlett Packard Enterprise Development LP
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
 # File: etcd_defrag.sh
 #
 # Examples:
@@ -48,17 +69,17 @@ echo "Skip defrag for: $SKIP_CLUSTER_NAME"
 
 if [ $DEFRAG_CLUSTER_NAME != "all" ]
 then
-  lines=$(kubectl get etcdcluster -A | awk '{print $1 "." $2}' | grep -v NAMESPACE | grep $DEFRAG_CLUSTER_NAME)
+  lines=$(kubectl get statefulsets.apps -A | grep bitnami-etcd | awk '{print $1 "." $2}' | grep $DEFRAG_CLUSTER_NAME)
   echo "etcd clusters found: $lines"
   for line in $lines
   do
       ns=$(echo $line | awk 'BEGIN { FS = "." } ; {print $1}')
       cluster=$(echo $line | awk 'BEGIN { FS = "." } ; {print $2}')
-      members=$(kubectl get etcdcluster -n $ns $cluster -o jsonpath='{.status.members.ready}' | sed 's/\[//g' | sed 's/\]//g')
+      members=$(kubectl get endpoints ${cluster} -n ${ns} -o jsonpath='{.subsets[*].addresses[*].targetRef.name}')
       for member in $members
       do
         echo "Defragging $member"
-        output=$(kubectl -n $ns exec -i $member -- /bin/sh -c 'ETCDCTL_API=3 etcdctl --command-timeout=600s defrag')
+        output=$(kubectl -n $ns exec -i $member -- /bin/sh -c 'etcdctl --command-timeout=600s defrag')
       done
   done
   echo "Completed"
@@ -66,20 +87,20 @@ else
   if [ $SKIP_CLUSTER_NAME != "none" ]
   then
     echo "Skipping defrag for: $SKIP_CLUSTER_NAME"
-    lines=$(kubectl get etcdcluster -A | awk '{print $1 "." $2}' | grep -v NAMESPACE | grep -v -w $SKIP_CLUSTER_NAME)
+    lines=$(kubectl get statefulsets.apps -A | grep bitnami-etcd | awk '{print $1 "." $2}' | grep -v -w $SKIP_CLUSTER_NAME)
   else
-    lines=$(kubectl get etcdcluster -A | awk '{print $1 "." $2}' | grep -v NAMESPACE)
+    lines=$(kubectl get statefulsets.apps -A | grep bitnami-etcd | awk '{print $1 "." $2}')
   fi
   for line in $lines
   do
       ns=$(echo $line | awk 'BEGIN { FS = "." } ; {print $1}')
       cluster=$(echo $line | awk 'BEGIN { FS = "." } ; {print $2}')
-      members=$(kubectl get etcdcluster -n $ns $cluster -o jsonpath='{.status.members.ready}' | sed 's/\[//g' | sed 's/\]//g')
+      members=$(kubectl get endpoints ${cluster} -n ${ns} -o jsonpath='{.subsets[*].addresses[*].targetRef.name}')
       for member in $members
       do
         echo "Defragging $member"
         if [ $cluster == "cray-vault-etcd" ]; then
-          output=$(kubectl -n $ns exec -i $member -- /bin/sh -c 'ETCDCTL_API=3 etcdctl --cacert /etc/etcdtls/operator/etcd-tls/etcd-client-ca.crt --cert /etc/etcdtls/operator/etcd-tls/etcd-client.crt --key /etc/etcdtls/operator/etcd-tls/etcd-client.key --endpoints https://localhost:2379 --command-timeout=600s defrag')
+          output=$(kubectl -n $ns exec -i $member -- /bin/sh -c 'etcdctl --cacert /etc/etcdtls/operator/etcd-tls/etcd-client-ca.crt --cert /etc/etcdtls/operator/etcd-tls/etcd-client.crt --key /etc/etcdtls/operator/etcd-tls/etcd-client.key --endpoints https://localhost:2379 --command-timeout=600s defrag')
         else
           output=$(kubectl -n $ns exec -i $member -- /bin/sh -c 'ETCDCTL_API=3 etcdctl --command-timeout=600s defrag')
         fi
@@ -87,6 +108,3 @@ else
   done
   echo "Completed"
 fi
-
-
-
