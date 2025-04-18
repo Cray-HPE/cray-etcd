85f9a8e33e243a1bfdc068a3ee12f08d83b392ac
Lindsay Eliasen <lindsay.eliasen@hpe.com>
2025-04-18 09:45:51 -0500

    CASMPET-7407 update test values

--- charts/cray-etcd-test/files/etcd-load.sh
+++ charts/cray-etcd-test/files/etcd-load.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Create a file with random text data
-dd if=/dev/urandom bs=1K count=512 | tr -dc 'a-zA-Z0-9' > data
+dd if=/dev/urandom bs=1K count=512 | tr -dc 'a-zA-Z0-9' > /tmp/data
 
 # Load the data into etcd
 for i in {1..8192}; do
   echo "$i"
-  etcdctl put "key-$i" "$(cat data)"
+  etcdctl put "key-$i" "$(cat /tmp/data)"
 done
 
 # Delete the data file
-rm data
+rm /tmp/data
